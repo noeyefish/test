e6eff553f2ce98d35129b41f935b434f6ee15f95
noeyefish <95496714+noeyefish@users.noreply.github.com>
2025-07-25 16:49:17 +0300

    popup.js

--- popup.js
+++ popup.js
@@ -14,7 +14,7 @@ fetch('https://raw.githubusercontent.com/RokuIL/Live-From-Israel/refs/heads/mast
         <span>${channel.Title}</span>
       `;
       div.addEventListener('click', () => {
-        window.open(`https://www.hlsplayer.org/?url=${encodeURIComponent(validStream)}`, '_blank');
+        window.open(`https://www.hlsplayer.org/play?url=${encodeURIComponent(validStream)}`, '_blank');
       });
       container.appendChild(div);
     });
